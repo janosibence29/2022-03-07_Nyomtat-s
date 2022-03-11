ebd743d642e5247e620f412859beb4c81d876e0e
janosibence29 <92314900+janosibence29@users.noreply.github.com>
2022-03-11 09:39:18 +0100

    7es feladat

--- nyomtatas.js
+++ nyomtatas.js
@@ -1,12 +1,15 @@
-function kalkulal(){
+function kalkulal() {
     //Űrlapadatok
-    const szelesseg=297;
-    const magassag=420;
-    const papir=document.getElementById('papirtipus').value;
+    const szelesseg = document.getElementById("szelesseg").value;
+    const magassag = document.getElementById("magassag").value;
+    const papir = document.getElementById("papirtipus").value;
     //Számítások
-    let terulet=Math.round((szelesseg*magassag)/10000);    
-    let koltseg=terulet*papir;
+    let terulet = Math.round((szelesseg * magassag) / 10000);
+    let koltseg = terulet * papir;
     //Megjelenítés
-    document.getElementById('valasz').style.visibility = "visible";
-}
+    document.getElementById("terulet").innerHTML = terulet;
+    document.getElementById("papir").innerHTML = papir;
+    document.getElementById("koltseg").innerHTML = koltseg;
+    document.getElementById("valasz").style.visibility = "visible";
+  }
 
